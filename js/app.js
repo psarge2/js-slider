2481466f23fdc0d71175262294179e08527f4af1
psarge2 <54369285+psarge2@users.noreply.github.com>
2021-04-03 22:11:14 -0400

    added init function to app.js which sets animation class to intiial slide

---
+++ js/app.js
@@ -0,0 +1,13 @@
+let init = () => {
+  let container = document.getElementsByClassName('jumbo-slider__container')[0],
+  slides = document.getElementsByClassName('jumbo-slider__slide'), 
+  circles = document.getElementsByClassName('jumbo-slider__circle'),
+  links = document.getElementsByClassName('jumbo-slider__link'),
+  current = 1,
+  time = 6000;
+
+  //add animation class to slide
+  slides[0].classList.add('jumbo-slider__slide--active');
+}
+
+init();
