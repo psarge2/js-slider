85ce93d19d13b61863a7cc2a1ae97255033ba97d
psarge2 <54369285+psarge2@users.noreply.github.com>
2021-04-04 22:57:32 -0400

    addes set interval function, updateNav function and start sliding function to intialize and run slides

--- js/app.js
+++ js/app.js
@@ -10,6 +10,36 @@ let init = () => {
   slides[0].classList.add('jumbo-slider__slide--active');
   links[current-1].classList.add('jumbo-slider__link--active');
   circles[current-1].classList.add('jumbo-slider__circle--filled');
+
+  //update elipsis and links
+  let updateNav = (current) => {
+    console.log(`update current: ${current}`)
+    for (let index = 0; index < slides.length; index++){
+      links[index].classList.remove('jumbo-slider__link--active');
+      circles[index].classList.remove('jumbo-slider__circle--filled');
+    }
+    links[current-1].classList.add('jumbo-slider__link--active');
+    circles[current-1].classList.add('jumbo-slider__circle--filled');
+  }
+  let startSliding = () => {
+    
+    setInterval(() => {
+      console.log(`current: ${current}`)
+      slides[1].classList.add('jumbo-slider__slide--active')
+      slides[0].classList.remove('jumbo-slider__slide--active')
+
+      console.log(`slides: ${slides.length}`)
+      if(current < slides.length){
+        current++
+        updateNav(current)
+      } else {
+        current = 1
+      }
+      container.appendChild(slides[0].cloneNode([true]));
+      container.removeChild(slides[0]);
+    }, 6000);
+  }
+  startSliding();
 }
 
 init();
